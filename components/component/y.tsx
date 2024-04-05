2f03763165b0d33da321047fb7d13bb85c178194
Apoorv Arora <apoorvaroranituk@gmail.com>
2024-04-05 20:30:23 +0530

    portffolio

--- components/component/y.tsx
+++ components/component/y.tsx
@@ -14,7 +14,6 @@ export function y() {
     <div className="flex flex-col min-h-[100dvh] dark">
       <header className="px-4 lg:px-6 h-14 flex items-center bg-gradient-to-r from-gray-800 to-gray-900">
         <Link className="flex items-center justify-center" href="#">
-          <MountainIcon className="h-8 w-8 text-white" />
           <span className="sr-only text-white">Acme Inc</span>
         </Link>
         <nav className="ml-auto flex gap-4 sm:gap-6">
@@ -250,25 +249,5 @@ export function y() {
         </nav>
       </footer>
     </div>
-  )
-}
-
-
-function MountainIcon(props) {
-  return (
-    <svg
-      {...props}
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    >
-      <path d="m8 3 4 8 5-5 5 15H2L8 3z" />
-    </svg>
   )
 }
